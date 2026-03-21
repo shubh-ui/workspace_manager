86cdc240ac701b552aac90f91053b0ebd3cb54a5
Shubham Patil <shubhpatil8510@gmail.com>
2026-03-21 13:47:42 +0530

    Added a HTTP status code config

---
+++ backend/src/config/http.config.ts
@@ -0,0 +1,24 @@
+const httpConfig = () => ({
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    METHOD_NOT_ALLOWED: 405,
+    CONFLICT: 409,
+    UNPROCESSABLE_ENTITY: 422,
+    TOO_MANY_REQUEST: 429,
+
+    INTERNAL_SERVER_ERROR: 500,
+    NOT_IMPLEMENTED: 501,
+    BAD_GATWAY: 502,
+    SERVICE_UNAVAILABLE: 503,
+    GATWAY_TIMEOUT: 504
+});
+
+export const HTTPSTATUS = httpConfig();
+
+export type HttpStatusCodeType = (typeof HTTPSTATUS)[keyof typeof HTTPSTATUS]
